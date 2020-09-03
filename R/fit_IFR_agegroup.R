79ba20fc823b020002a6d7c3175fda97e7691bcd
Joel Hellewell <j.hellewell14@imperial.ac.uk>
2020-09-03 17:55:58 +0100

    Wrote up IFR estimates from different sources. Wrote code to fit a model logit(IFR) ~ age group midpoint Doesn't fit well at higher age groups.

---
+++ R/fit_IFR_agegroup.R
@@ -0,0 +1,57 @@
+df <- fread("~/repos/uk_inf_curve/ifr_data.csv")
+
+library(ggplot2)
+library(magrittr)
+library(data.table)
+library(patchwork)
+library(MASS)
+
+df[, agemid := (agehigh + agelow) / 2] 
+
+fit <- rlm(data = df, boot::logit(middle / 100) ~ agemid, weights = mean_ci, wt.method = "inv.var")
+
+preds <- predict(fit, newdata = data.frame(agemid = seq(2, 95, 1)), se.fit = TRUE)
+preds <- data.table(x = seq(2, 95, 1), 
+                    y = preds$fit, 
+                    ymin = preds$fit - 1.96 * preds$se.fit,
+                    ymax = preds$fit + 1.96 * preds$se.fit)
+
+p1 <- df %>%
+  ggplot(aes(x = agemid, 
+             y = log(middle / 100), 
+             col = author,
+             ymin = log(bottom / 100),
+             ymax = log(top / 100))) + 
+  geom_point() +
+  geom_line() +
+  geom_errorbar() +
+  geom_line(inherit.aes = FALSE, data = preds, aes(x = x, y = y), col = "blue", size = 1.2) +
+  geom_ribbon(inherit.aes = FALSE, data = preds, aes(x = x, ymin = ymin, ymax = ymax), alpha = 0.2) +
+  cowplot::theme_cowplot() +
+  labs(y = "logit(Infection Fatality Ratio)",
+       x = "Age group mid-point")
+
+p2 <- df %>%
+  ggplot(aes(x = agemid,
+             y = middle,
+             col = author,
+             ymin = bottom,
+             ymax = top)) +
+  geom_point() +
+  geom_line() +
+  geom_errorbar() +
+  geom_line(inherit.aes = FALSE, data = preds, 
+            aes(x = x, y = boot::inv.logit(y)* 100), col = "blue", size = 1.2) +
+  geom_ribbon(inherit.aes = FALSE, data = preds, 
+              aes(x = x, ymin = boot::inv.logit(ymin) * 100, 
+                  ymax = boot::inv.logit(ymax) * 100), alpha = 0.2) +
+  cowplot::theme_cowplot() +
+  labs(y = "Infection Fatality Ratio (%)",
+       x = "Age group mid-point")
+
+p1 + p2
+
+
+
+
+
